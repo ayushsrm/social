e986a87ecccc5ccb8036efbacf195f641d89be31
Jay Prakash <jaysingha261@gmail.com>
2023-06-10 11:13:46 +0530

    complete card.js

--- components/Card.js
+++ components/Card.js
@@ -1,6 +1,6 @@
 export default function Card({children}){
     return(
-        <div className ="bg-white shadow-md">
+        <div className ="bg-white shadow-md shadow-gray-300 rounded-md p-4 mb-5">
             {children}
             </div>
     )
