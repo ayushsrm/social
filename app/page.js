7427989eb67d9d0f2123ef29d7d8f5dd8b97d8e2
ar7692@srmist.edu.in <AYUSH>
2023-06-10 09:40:53 +0530

    init commit

--- app/page.js
+++ app/page.js
@@ -2,16 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
- <nav className='px-4 py-4 bg-purple-900 text-white '>
-  <ul className='flex justify-end'>
-    <li className='mx-4 cursor-pointer'>
-     home
-    </li>
 
-  </ul>
-
-
- </nav>
 
    
   )
