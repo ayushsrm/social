43704ef3f4137f5c8ecd956f257636fcaa5d78aa
ar7692@srmist.edu.in <AYUSH>
2023-06-10 11:22:26 +0530

    page changes

--- app/page.js
+++ app/page.js
@@ -1,9 +1,22 @@
 import Image from 'next/image'
+import Card from '@/components/Card'
 
 export default function Home() {
   return (
+      <div className='flex mt-4 max-w-4xl mx-auto '>
+        <div className='w-1/3'>
+         <Card>links</Card>
+          
+        </div>
+
+        <div className=''>
+          </div>
 
 
+          </div>
+        
+     
+        
    
   )
 }
