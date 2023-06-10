e536b2d991f6f4803fe725251379cbc2825a0060
Jay Prakash <jaysingha261@gmail.com>
2023-06-10 12:35:16 +0530

    A

--- app/page.js
+++ app/page.js
@@ -1,15 +1,18 @@
 import Image from 'next/image'
 import Card from '@/components/Card'
+import NavigationCard from '@/components/NavigationCard'
 
 export default function Home() {
   return (
-      <div className='flex mt-4 max-w-4xl mx-auto '>
+      <div className='flex mt-4 max-w-4xl mx-auto gap-4'>
         <div className='w-1/3'>
-         <Card>links</Card>
-          
+          <NavigationCard/>
         </div>
 
-        <div className=''>
+        <div className='grow'>
+          <Card>form here</Card>
+          <Card>first post here</Card>
+        
           </div>
 
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,15 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        socialBg: '#F5F7FB',
+        socialBlue: '218DFA',
       },
+      // backgroundImage: {
+      //   'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      //   'gradient-conic':
+      //     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      //},
     },
   },
   plugins: [],
