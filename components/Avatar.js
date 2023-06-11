2776dad27a0ea3eaa207851e6047d5d3a3d41751
ar7692@srmist.edu.in <AYUSH>
2023-06-11 12:43:15 +0530

    profile done

--- components/Avatar.js
+++ components/Avatar.js
@@ -1,13 +1,13 @@
 
 export default function Avatar(size){
-    let width = 'w-12';
+    let width = 'w-16';
     if (size === 'lg') {
       width = 'w-36';
     }
     return(
         <div className={`${width} rounded-full overflow-auto block`}>
         
-    <img src="https://images.prismic.io/ocean-agency-cms/10000df0-7409-496d-83ba-8ed6ed7d4815_Ocean-Decade-Exhibition---45-Credit-Aanish-Peshave.jpg?auto=compress,format&w=750" alt="?" className="w-full" />
+    <img src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="?" className="w-full" />
 
         
         </div>
