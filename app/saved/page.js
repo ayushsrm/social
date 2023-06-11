89cd5f9a4b72d7d112a6af2b73a2aa9c438d9f27
Jay Prakash <jaysingha261@gmail.com>
2023-06-11 14:02:42 +0530

    Notifications Done

--- app/saved/page.js
+++ app/saved/page.js
@@ -1,9 +1,10 @@
 import PostCard from "@/components/PostCard";
+import Layout from "@/components/layout";
 
 export default function SavedPosts(){
     return(
         <Layout>
-            <h1>Your saved posts</h1>
+            <h1 className ='text-6xl mb-4 text-gray-300 '>Saved posts</h1>
             <PostCard/>
             <PostCard/>
         </Layout>
