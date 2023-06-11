e2bf78d3b95fb9d61e317c13744dede0ba324378
Jay Prakash <jaysingha261@gmail.com>
2023-06-11 15:43:30 +0530

    Login Done

--- components/Card.js
+++ components/Card.js
@@ -1,6 +1,6 @@
 export default function Card({children,noPadding}){
 
-    let classes ='bg-white shadow-md shadow-gray-300 rounded-md p-4 mb-5 overflow-hidden';
+    let classes ='bg-white shadow-md shadow-gray-300 rounded-md mb-5 overflow-hidden';
 if(!noPadding){
     classes +='p-4';
 }
--- components/layout.js
+++ components/layout.js
@@ -4,14 +4,16 @@ import NavigationCard from '@/components/NavigationCard'
 // import PostFormCard from '@/components/PostFormCard'
 // import PostCard from '@/components/PostCard'
 
-export default function Layout({children}) {
+export default function Layout({children, hideNavigation}) {
   return (
       <div className='flex mt-4 max-w-4xl mx-auto gap-4'>
+        {!hideNavigation && (
         <div className='w-1/3'>
           <NavigationCard/>
         </div>
+        )}
 
-        <div className='w-9/12'>
+        <div className={hideNavigation ? 'w-full' : 'w-9/12'}>
           {children}
           </div>
 
